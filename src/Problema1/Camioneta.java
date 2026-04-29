63b9ca74a413782afa8e9479878f0be9b59be0c9
Daniel Muñoz Mendoza <munozmendozadaniel4@gmail.com>
2026-04-29 17:38:38 -0600

    Update Camioneta.java

--- src/Problema1/Camioneta.java
+++ src/Problema1/Camioneta.java
@@ -1,4 +1,37 @@
 package Problema1;
 
-public class Camioneta {
+public class Camioneta extends Vehiculo {
+    private String tipoTraccion;
+    private double capacidadCarga;
+    private int filasAsientos;
+
+    public Camioneta(String placa, String marca, String modelo, int año, double costoPorDia, String tipoTraccion, double capacidadCarga, int filasAsientos) {
+        super(placa, marca, modelo, año, costoPorDia);
+        this.tipoTraccion = tipoTraccion;
+        this.capacidadCarga = capacidadCarga;
+        this.filasAsientos = filasAsientos;
+    }
+
+    public String getTipoTraccion() {
+        return this.tipoTraccion;
+    }
+
+    public double getCapacidadCarga() {
+        return this.capacidadCarga;
+    }
+
+    public int getFilasAsientos() {
+        return this.filasAsientos;
+    }
+
+    public double calcularCosto(int dias) {
+        double total = super.calcularCosto(dias);
+        if (this.tipoTraccion.equals("4x4")) {
+            total *= 1.15;
+        }
+
+        return total;
+    }
+
 }
+
