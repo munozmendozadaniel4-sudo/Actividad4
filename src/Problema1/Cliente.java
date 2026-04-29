65271bb335e8fba74dc842b6f97996358e91b480
Daniel Muñoz Mendoza <munozmendozadaniel4@gmail.com>
2026-04-29 17:39:04 -0600

    Update Cliente.java

--- src/Problema1/Cliente.java
+++ src/Problema1/Cliente.java
@@ -1,4 +1,30 @@
 package Problema1;
 
 public class Cliente {
+    private String nombre;
+    private String celular;
+    private String identificacion;
+
+    public Cliente(String nombre, String celular, String identificacion) {
+        this.nombre = nombre;
+        this.celular = celular;
+        this.identificacion = identificacion;
+    }
+
+    public String getNombre() {
+        return this.nombre;
+    }
+
+    public String getCelular() {
+        return this.celular;
+    }
+
+    public String getIdentificacion() {
+        return this.identificacion;
+    }
+
+    public String toString() {
+        return this.nombre;
+    }
 }
+
