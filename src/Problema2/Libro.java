0081835f494d1af6d87522059f3bb57602855e5d
Daniel Muñoz Mendoza <munozmendozadaniel4@gmail.com>
2026-04-29 17:42:13 -0600

    Update Libro.java

--- src/Problema2/Libro.java
+++ src/Problema2/Libro.java
@@ -1,4 +1,26 @@
 package Problema2;
 
-public class Libro {
+public class Libro extends Material {
+    private String genero;
+    private String editorial;
+
+
+    public Libro(String codigo, String titulo, String autor, int añoPublicacion, int numeroPaginas, String genero, String editorial) {
+        super(codigo, titulo, autor, añoPublicacion, numeroPaginas);
+        this.genero = genero;
+        this.editorial = editorial;
+    }
+
+    public String getGenero() {
+        return this.genero;
+    }
+
+    public String getEditorial() {
+        return this.editorial;
+    }
+
+    public String toString() {
+        String var10000 = super.toString();
+        return var10000 + ", Genero: " + this.genero + ", Editorial: " + this.editorial;
+    }
 }
