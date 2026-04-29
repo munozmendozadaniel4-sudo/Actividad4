ba65ac735d879628a84abf4eae4beddd2b24f59e
Daniel Muñoz Mendoza <munozmendozadaniel4@gmail.com>
2026-04-29 17:39:53 -0600

    Update Motocicleta.java

--- src/Problema1/Motocicleta.java
+++ src/Problema1/Motocicleta.java
@@ -1,4 +1,33 @@
 package Problema1;
 
-public class Motocicleta {
+public class Motocicleta extends Vehiculo {
+    private int cilindrada;
+    private String tipo;
+    private boolean incluyeCasco;
+
+    public Motocicleta(String placa, String marca, String modelo, int año, double costoPorDia, int cilindrada, String tipo, boolean incluyeCasco) {
+        super(placa, marca, modelo, año, costoPorDia);
+        this.cilindrada = cilindrada;
+        this.tipo = tipo;
+        this.incluyeCasco = incluyeCasco;
+    }
+
+    public int getCilindrada() {
+        return this.cilindrada;
+    }
+
+    public String getTipo() {
+        return this.tipo;
+    }
+
+    public boolean getIncluyeCasco() {
+        return this.incluyeCasco;
+    }
+
+    public double calcularCosto(int dias) {
+        double total = super.calcularCosto(dias);
+        return total * 0.95;
+    }
+
 }
+
