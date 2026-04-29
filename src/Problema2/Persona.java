e7b142b57f62c0f5309a2f0af85e1723e1e86cdd
Daniel Muñoz Mendoza <munozmendozadaniel4@gmail.com>
2026-04-29 17:43:08 -0600

    Update Persona.java

--- src/Problema2/Persona.java
+++ src/Problema2/Persona.java
@@ -1,4 +1,35 @@
 package Problema2;
 
 public class Persona {
+    private String nombre;
+    private String identificacion;
+    private String celular;
+    private String direccion;
+
+    public Persona(String nombre, String identificacion, String celular, String direccion) {
+        this.nombre = nombre;
+        this.identificacion = identificacion;
+        this.celular = celular;
+        this.direccion = direccion;
+    }
+
+    public String getNombre() {
+        return this.nombre;
+    }
+
+    public String getIdentificacion() {
+        return this.identificacion;
+    }
+
+    public String getCelular() {
+        return this.celular;
+    }
+
+    public String getDireccion() {
+        return this.direccion;
+    }
+
+    public String toString() {
+        return this.nombre + " (" + this.identificacion + ")";
+    }
 }
