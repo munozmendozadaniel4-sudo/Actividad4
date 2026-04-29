22a7b5b071ccac6c0cc9049bf9e6110949406ae3
Daniel Muñoz Mendoza <munozmendozadaniel4@gmail.com>
2026-04-29 17:42:42 -0600

    Update Material.java

--- src/Problema2/Material.java
+++ src/Problema2/Material.java
@@ -1,4 +1,61 @@
 package Problema2;
 
 public class Material {
+    protected String codigo;
+    protected String titulo;
+    protected String autor;
+    protected String estado;
+    protected int añoPublicacion;
+    protected int numeroPaginas;
+
+
+    public Material(String codigo, String titulo, String autor, int añoPublicacion, int numeroPaginas) {
+        this.codigo = codigo;
+        this.titulo = titulo;
+        this.autor = autor;
+        this.añoPublicacion = añoPublicacion;
+        this.numeroPaginas = numeroPaginas;
+        this.estado = "Disponible";
+    }
+
+    public String getCodigo() {
+        return this.codigo;
+    }
+
+    public String getTitulo() {
+        return this.titulo;
+    }
+
+    public String getAutor() {
+        return this.autor;
+    }
+
+    public int getAñoPublicacion() {
+        return this.añoPublicacion;
+    }
+
+    public int getNumeroPaginas() {
+        return this.numeroPaginas;
+    }
+
+    public String getEstado() {
+        return this.estado;
+    }
+
+    public boolean prestar() {
+        if (this.estado.equals("Disponible")) {
+            this.estado = "Prestado";
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public void devolver() {
+        this.estado = "Disponible";
+    }
+
+    public String toString() {
+        return this.titulo + " (" + this.estado + ")";
+    }
 }
