fb7a1eb98792c39ea052228a2922a008c32a9e94
Daniel Muñoz Mendoza <munozmendozadaniel4@gmail.com>
2026-04-29 17:44:06 -0600

    Update Revista.java

--- src/Problema2/Revista.java
+++ src/Problema2/Revista.java
@@ -1,4 +1,27 @@
 package Problema2;
 
-public class Revista {
+public class Revista extends Material {
+    private int numeroEdicion;
+    private String mesPublicacion;
+
+
+    public Revista(String codigo, String titulo, String autor, int añoPublicacion, int numeroPaginas, int numeroEdicion, String mesPublicacion) {
+        super(codigo, titulo, autor, añoPublicacion, numeroPaginas);
+        this.numeroEdicion = numeroEdicion;
+        this.mesPublicacion = mesPublicacion;
+    }
+
+    public int getNumeroEdicion() {
+        return this.numeroEdicion;
+    }
+
+    public String getMesPublicacion() {
+        return this.mesPublicacion;
+    }
+
+    public String toString() {
+        String var10000 = super.toString();
+        return var10000 + ", Edicion: " + this.numeroEdicion + ", Mes: " + this.mesPublicacion;
+    }
 }
+
